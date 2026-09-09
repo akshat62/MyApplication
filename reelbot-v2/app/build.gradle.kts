c7540276142ec174abacce65995bf5a0b8231c33
Akshat Gupta <akshat.gupta24@gmail.com>
2026-09-09 19:39:25 +0530

    Remove obsolete Kotlin Android plugin for AGP 9

--- reelbot-v2/app/build.gradle.kts
+++ reelbot-v2/app/build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -23,8 +20,6 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 }
-
-kotlin { compilerOptions { jvmTarget.set(JvmTarget.JVM_17) } }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.16.0")
