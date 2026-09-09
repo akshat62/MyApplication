ccaaa8c960363f0d786ea3894b0feab8415278bb
Akshat Gupta <akshat.gupta24@gmail.com>
2026-09-10 04:47:15 +0530

    Fix Kotlin 2 Compose plugin for ReelBot V3

---
+++ reelbot-v3-overrides/build.gradle.kts
@@ -0,0 +1,7 @@
+// Build override for CI: Kotlin 2.x requires the Compose compiler Gradle plugin.
+plugins {
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+}
